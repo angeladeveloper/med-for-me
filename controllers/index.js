99448f45cafd529bce7fe43d16d40fb8d44156ad
Angela W <squittenplease@gmail.com>
2022-07-10 10:27:01 -0700

    got /kitten up and running 🚀
    go to http://localhost:3001/kitten and HOPEFULLY, you'll see someone. 🙀
    
    But also, I was making sure the routes worked.

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
-
+router.get('/kitten', (req, res) => {
+  try {
+    res.render('kitten')
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 module.exports = router;
