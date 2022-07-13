3b9a8f9e1ab957d3a71f7fcfa2d6fcda29e3d8ac
wushuangster <wushuangster@hotmail.com>
2022-07-12 22:00:55 -0700

    modifed index

---
+++ models/Medication.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class medication extends Model {
+ 
+}
+
+medication.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    med_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    med_descrip: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    
+   med_generic_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    med_dozes:{
+      
+    },
+    med_dozes_frequency:{
+
+    },
+    
+
+  },
+  {
+   
+
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'medication',
+  }
+);
+
+module.exports = medication;
